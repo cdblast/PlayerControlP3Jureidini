1fb033b1727bc9ffb82b296501e24285e070e0c0
Lucille Amanda Jureidini <145380670+cdblast@users.noreply.github.com>
2024-02-29 10:56:19 -0800

    All Finished!
    Added MoveForward script which made oncoming vehicles possible
    Added Camera switching to playercontrol script (functional!!!!!!! huzzah)

--- Create with Code/PrototypeJureidiniP3/Assets/Scripts/PlayerController.cs
+++ Create with Code/PrototypeJureidiniP3/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public Camera mainCamera;
+    public Camera hoodCamera;
+    public KeyCode switchKey;
     private float speed = 50.0f;
     private float turnSpeed = 75.0f;
     private float horizontalInput;
@@ -24,5 +27,11 @@ public class PlayerController : MonoBehaviour
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
         //Rotates the vehicle based on horizontal input
         transform.Rotate(Vector2.up, turnSpeed * horizontalInput * Time.deltaTime);
+
+        if(Input.GetKey(switchKey))
+        {
+            mainCamera.enabled = !mainCamera.enabled;
+            hoodCamera.enabled = !hoodCamera.enabled;
+        }
     }
 }
